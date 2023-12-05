024906a492da6e85da053ec2785b3de42781e658
leilayesufu <leilayesufu2002@gmail.com>
2023-12-05 16:32:40 +0100

    update

--- model/framework/run.sh
+++ model/framework/run.sh
@@ -1,1 +1,1 @@
-bash $1/code/Table1_entry1/Script_for_Reproduce_result_entry1.sh $2 $3
+bash $1/code/Table1_entry1/Script_for_Reproduce_result_entry1.sh -i $2code/Table1_entry1 -o $3code/Table1_entry1
