a1b948021325ae67415e97042b77173fae6f0760
Malik badmus <malik.badmus@icloud.com>
2024-03-23 10:37:59 +0100

    final update

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -46,6 +46,7 @@ assert input_len == output_len
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow((["activity_10", "activity_20", "activity_40", "activity_60", "activity_80", "activity_100"]))  # header
+    writer.writerow((["activity"]))  # header
     for o in outputs:
-        writer.writerow(o)
+        writer.writerow([o[0]])
+        
